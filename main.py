f69ead38c43e4bccc21d54e7bb21357d13f9195c
smcox13 <thestevencox@gmail.com>
2025-01-28 20:31:11 -0500

    before containers

--- main.py
+++ main.py
@@ -11,16 +11,21 @@ def main():
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    p1 = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     dt = 0
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
             
-        screen.fill(0)
-        p1 = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+        p1.update(dt)
+
+        screen.fill("black")
         p1.draw(screen)
         pygame.display.flip()
 
